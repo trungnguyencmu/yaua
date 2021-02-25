6611f9fe880756ac76b22b09a748e03e23160106
Trung <nguyen.quoc.trung@photobank.co.jp>
2021-02-25 14:45:21 +0700

    add CDN

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
-import * as iam from '@aws-cdk/aws-iam';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -8,18 +8,23 @@ export class InfrastructureStack extends cdk.Stack {
 
     const bucket = new s3.Bucket(this, 'WebsiteBucket', {
       bucketName: 'yaua',
-      websiteIndexDocument: 'index.html', // 1
-      blockPublicAccess: new s3.BlockPublicAccess({ restrictPublicBuckets: false }) // 2
+      websiteIndexDocument: 'index.html',
     });
 
-    // 3
-    const bucketPolicy = new iam.PolicyStatement({
-      actions: ['s3:GetObject'],
-      resources: [
-        `${bucket.bucketArn}/*`
-      ],
-      principals: [new iam.Anyone()],
+    const cloudFrontOAI = new cloudfront.OriginAccessIdentity(this, 'OAI');
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'MyDistribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentity: cloudFrontOAI,
+          },
+          behaviors: [{ isDefaultBehavior: true }]
+        }
+      ]
     })
-    bucket.addToResourcePolicy(bucketPolicy); // 4
+
+    bucket.grantRead(cloudFrontOAI.grantPrincipal);
   }
 }
